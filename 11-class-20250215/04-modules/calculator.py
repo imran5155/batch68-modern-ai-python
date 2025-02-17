08e1cb3a1a3e7f7941088932a11e71f76fdc2193
rehan-ul-haq <mmrhaq@gmail.com>
2025-02-17 10:56:55 +0500

    Add class 11

---
+++ 11-class-20250215/04-modules/calculator.py
@@ -0,0 +1,13 @@
+def Add (first_number:int, second_number:int) -> int:
+    return first_number + second_number
+
+def multiply (first_number:int, second_number:int) -> int:
+    return first_number * second_number
+
+
+def divide (first_number:int, second_number:int) -> float:
+    return first_number / second_number
+
+
+def subtract (first_number:int, second_number:int) -> int:
+    return first_number - second_number
---
+++ 11-class-20250215/04-modules/main.py
@@ -0,0 +1,8 @@
+import calculator
+
+
+result = calculator.Add(5, 9)
+print(f"Addition Result: {result}")
+
+
+# TODOS: Now try to implement single function, two functions only and so on.
